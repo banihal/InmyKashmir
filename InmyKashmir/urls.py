e205d027dfe7a4255c6f54093d24e4821a9ac3d1
banihal <imtiyazbanihali@gmail.com>
2020-12-03 22:32:20 +0530

    make it better

--- InmyKashmir/urls.py
+++ InmyKashmir/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from . import settings
+from django.contrib.staticfiles.urls import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from home import views
 
 urlpatterns = [
@@ -26,6 +27,6 @@ urlpatterns = [
     path('about/',views.about, name='about'),
     path('admin/', admin.site.urls),
    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
